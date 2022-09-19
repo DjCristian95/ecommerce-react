0e85a769e78923db982c3af6dc6810715f1707f5
Cristian Murcia <cristian47@gmail.com>
2022-09-19 20:27:58 -0300

    Comienzo CartContext

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,25 @@ import './App.css';
 import NavbarComponent from './components/Navbar/navbarComponent';
 import ItemListContainer from './container/itemListContainer';
 import ItemDetailContainer from './container/itemDetailContainer';
-import { useState} from 'react';
+import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CartContext from './contexts/cartContext';
 
 function App() {
   const [cart, setCart] = useState([]);
 
   return (
-    <BrowserRouter>
-      <NavbarComponent cart={cart} setCart={setCart} />
-      <Routes>
-        <Route path="/" element={<ItemListContainer />} />
-        <Route path="/producto/:idProducto" element={<ItemListContainer />} />
-        <Route path="/categoria/:idCategoria" element={<ItemListContainer />} />
-        <Route path="/detalle/:idProducto" element={<ItemDetailContainer />} />
-      </Routes>
-    </BrowserRouter>
+    <CartContext.Provider>
+      <BrowserRouter>
+        <NavbarComponent cart={cart} setCart={setCart} />
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/producto/:idProducto" element={<ItemListContainer />} />
+          <Route path="/categoria/:idCategoria" element={<ItemListContainer />} />
+          <Route path="/detalle/:key" element={<ItemDetailContainer />} />
+        </Routes>
+      </BrowserRouter>
+    </CartContext.Provider>
   );
 }
 
