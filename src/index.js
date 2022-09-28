29352a17d16f58925cf9b5e2464f4cb4e2c4b83f
Cristian Murcia <cristian47@gmail.com>
2022-09-28 19:50:38 -0300

    Conexion con Firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { initializeApp } from "firebase/app";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,3 +17,19 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQeSp5_vSG0UpHOJdrRn-N02XfuMpJq08",
+  authDomain: "ecommerce-react-cc9b9.firebaseapp.com",
+  projectId: "ecommerce-react-cc9b9",
+  storageBucket: "ecommerce-react-cc9b9.appspot.com",
+  messagingSenderId: "487881868918",
+  appId: "1:487881868918:web:bc3350b4b3d94ca4728157"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
